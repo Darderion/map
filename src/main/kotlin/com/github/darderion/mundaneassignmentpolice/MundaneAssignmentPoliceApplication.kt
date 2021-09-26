4694668fc839c658a3619dd33073def1eea32dfd
Darderion <Eobard1SA@gmail.com>
2021-09-26 19:53:52 +0300

    Created a repository

---
+++ src/main/kotlin/com/github/darderion/mundaneassignmentpolice/MundaneAssignmentPoliceApplication.kt
@@ -0,0 +1,11 @@
+package com.github.darderion.mundaneassignmentpolice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MundaneAssignmentPoliceApplication
+
+fun main(args: Array<String>) {
+	runApplication<MundaneAssignmentPoliceApplication>(*args)
+}
