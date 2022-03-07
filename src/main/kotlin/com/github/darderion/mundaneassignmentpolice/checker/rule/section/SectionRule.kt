2b1ad981fc4e1b3963a033bca6caad38b3b4a503
Pavel <pavsaltykov@mail.ru>
2022-03-07 16:56:26 +0300

    implemented SectionRule

--- src/main/kotlin/com/github/darderion/mundaneassignmentpolice/checker/rule/section/SectionRule.kt
+++ src/main/kotlin/com/github/darderion/mundaneassignmentpolice/checker/rule/section/SectionRule.kt
@@ -6,17 +6,22 @@ import com.github.darderion.mundaneassignmentpolice.checker.rule.Rule
 import com.github.darderion.mundaneassignmentpolice.pdfdocument.PDFArea
 import com.github.darderion.mundaneassignmentpolice.pdfdocument.PDFDocument
 import com.github.darderion.mundaneassignmentpolice.pdfdocument.PDFRegion.Companion.NOWHERE
-import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Line
 import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Section
 
 class SectionRule(
-    val predicates: List<(sections: List<Section>) -> List<Line>>,
+    val predicates: List<(sections: List<Section>) -> List<Section>>,
     type: RuleViolationType,
     name: String
 ): Rule(NOWHERE.except(PDFArea.SECTION), name, type) {
     override fun process(document: PDFDocument): List<RuleViolation> {
         val ruleViolations: MutableSet<RuleViolation> = mutableSetOf()
-
+        val sections = document.areas!!.sections
+        predicates.forEach { predicate ->
+                predicate(sections)
+                    .map {document.text.slice(it.titleIndex until it.contentIndex) }
+                    .map { RuleViolation(it, name, type) }
+                    .forEach { ruleViolations.add(it) }
+        }
         return ruleViolations.toList()
     }
 }
--- src/main/kotlin/com/github/darderion/mundaneassignmentpolice/checker/rule/section/SectionRuleBuilder.kt
+++ src/main/kotlin/com/github/darderion/mundaneassignmentpolice/checker/rule/section/SectionRuleBuilder.kt
@@ -1,15 +1,14 @@
 package com.github.darderion.mundaneassignmentpolice.checker.rule.section
 
 import com.github.darderion.mundaneassignmentpolice.checker.RuleViolationType
-import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Line
 import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Section
 
 class SectionRuleBuilder {
-    private val predicates: MutableList<(list: List<Section>) -> List<Line>> = mutableListOf()
+    private val predicates: MutableList<(list: List<Section>) -> List<Section>> = mutableListOf()
     private var type: RuleViolationType = RuleViolationType.Error
     private var name: String = "Rule name"
 
-    fun disallow(predicate: (list: List<Section>) -> List<Line>) = this.also { predicates.add(predicate) }
+    fun disallow(predicate: (list: List<Section>) -> List<Section>) = this.also { predicates.add(predicate) }
 
     fun called(name: String) = this.also { this.name = name }
 
