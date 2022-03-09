94c03ec8b027f340d367135ecf2390b1301e13f6
Pavel <pavsaltykov@mail.ru>
2022-03-09 23:20:16 +0300

    modified UrlUtil

--- src/main/kotlin/com/github/darderion/mundaneassignmentpolice/utils/UrlUtil.kt
+++ src/main/kotlin/com/github/darderion/mundaneassignmentpolice/utils/UrlUtil.kt
@@ -1,37 +1,60 @@
 package com.github.darderion.mundaneassignmentpolice.utils
 
 import java.net.HttpURLConnection
+import java.net.MalformedURLException
 import java.net.URL
 
 class UrlUtil {
     companion object {
-        fun isShortened(url: URL) = !expand(url).host.contains(url.host, true)
-
-
-        fun expand(shortenedUrl: URL): URL {
-            val connection = shortenedUrl.openConnection() as HttpURLConnection
+        fun getUrl(url: String) =
             try {
-                with(connection) {
-                    instanceFollowRedirects = true
-                    requestMethod = "HEAD"
-                    connect()
-                    return when (responseCode) {
-                        301 -> {  // HttpURLConnection doesn't automatically follow redirects from a protocol to another
-                            val redirect = headerFields.filter {
-                                it.key.equals("location", true)
-                            }.map { it.value.first() }.first()
-                            expand(URL(redirect))
-                        }
-                        HttpURLConnection.HTTP_OK -> this.url
-                        else -> throw UnsupportedOperationException("Unexpected HTTP status code")
-                    }
-                }
-            } catch (e: Exception) {
-                throw UnsupportedOperationException(e)
+                URL(url)
+            } catch (e: MalformedURLException) {
+                throw IllegalArgumentException("Incorrect url: $url", e)
             }
-            finally {
+
+        fun isShortened(url: String): Boolean {
+            val currentUrl = getUrl(url)
+            val expandedUrl = getUrl(expand(url))
+            return !expandedUrl.host.contains(currentUrl.host, true)
+        }
+
+        fun expand(shortenedUrl: String): String {
+            val url = getUrl(shortenedUrl)
+            val connection = url.openConnection() as HttpURLConnection
+            try {
+               return getExpandedUrl(connection)
+            } catch (e: Exception) {
+                when (e) {
+                    is InvalidOperationException -> throw e
+                    else -> throw InvalidOperationException(e)
+                }
+            } finally {
                 connection.disconnect()
             }
         }
+
+        private fun getExpandedUrl(connection: HttpURLConnection): String {
+            with(connection) {
+                instanceFollowRedirects = true
+                requestMethod = "HEAD"
+                connect()
+                return when (responseCode) {
+                    301 -> {  // HttpURLConnection doesn't automatically follow redirects from a protocol to another
+                        val redirect = headerFields.filter {
+                            it.key.equals("location", true)
+                        }.map { it.value.first() }.first()
+                        expand(redirect)
+                    }
+                    HttpURLConnection.HTTP_OK -> this.url.toString()
+                    else -> throw InvalidOperationException("Unexpected HTTP status code")
+                }
+            }
+        }
     }
+}
+
+class InvalidOperationException: UnsupportedOperationException {
+    constructor(message: String): super(message)
+    constructor(cause: Throwable): super(cause)
 }
