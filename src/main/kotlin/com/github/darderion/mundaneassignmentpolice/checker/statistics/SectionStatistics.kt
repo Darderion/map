ff5c772c394bec6b28b50f48107019e2ac136767
liana <liana300291@gmail.com>
2022-04-07 14:23:24 +0300

    statistics service added, stop words added to text file

--- src/main/kotlin/com/github/darderion/mundaneassignmentpolice/checker/statistics/SectionStatistics.kt
+++
@@ -1,6 +0,0 @@
-package com.github.darderion.mundaneassignmentpolice.checker.statistics
-import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Section
-
-data class SectionStatistics(var section: Section, var sectionSizeInPage: Int,
-                             var sectionSizeInPercents: Float, var documentSize:Int)
-
