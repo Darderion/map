b1b157ea6aa3095a18484b6a2570d7042bec6d18
alex-tinarsky <tinarsky@gmail.com>
2022-04-11 18:46:55 +0300

    Refactoring: added enum for section's titles, some comments

---
+++ src/main/kotlin/com/github/darderion/mundaneassignmentpolice/checker/Section.kt
@@ -0,0 +1,11 @@
+package com.github.darderion.mundaneassignmentpolice.checker
+
+enum class Section(val title: String) {
+	TABLE_OF_CONTENT("Оглавление"),
+	INTRODUCTION("Введение"),
+	PROBLEM_STATEMENT("Постановка задачи"),
+	REVIEW("Обзор"),
+	CONTENT("Контент"),
+	CONCLUSION("Заключение"),
+	BIBLIOGRAPHY("Список литературы")
+}
--- src/main/kotlin/com/github/darderion/mundaneassignmentpolice/rules/Rules.kt
+++ src/main/kotlin/com/github/darderion/mundaneassignmentpolice/rules/Rules.kt
@@ -1,6 +1,7 @@
 package com.github.darderion.mundaneassignmentpolice.rules
 
 import com.github.darderion.mundaneassignmentpolice.checker.RuleViolationType
+import com.github.darderion.mundaneassignmentpolice.checker.Section
 import com.github.darderion.mundaneassignmentpolice.checker.rule.list.ListRuleBuilder
 import com.github.darderion.mundaneassignmentpolice.checker.rule.regex.RegexRuleBuilder
 import com.github.darderion.mundaneassignmentpolice.checker.rule.symbol.SymbolRuleBuilder
@@ -174,8 +175,9 @@ val RULE_TABLE_OF_CONTENT_NUMBERS = TableOfContentRuleBuilder()
 		it.filter {
 			// println("${it.text.count()} -> ${it.content}")
 			val text = it.text.filter { it.text.trim().isNotEmpty() }
-			((text.count() == 3 && (text[1].text == "Введение" || text[1].text == "Заключение")) ||
-					(text.count() == 4 && text[1].text == "Список" && text[2].text == "литературы"))
+			((text.count() == 3 && (text[1].text == Section.INTRODUCTION.title ||
+					text[1].text == Section.CONCLUSION.title)) ||
+					(text.count() == 4 && (text[1].text + " " + text[2].text) == Section.BIBLIOGRAPHY.title))
 		}
 	}.called("Введение, заключение и список литературы не нумеруются")
 	.getRule()
@@ -192,12 +194,12 @@ val RULE_SYMBOLS_IN_SECTION_NAMES = TableOfContentRuleBuilder()
 	.getRule()
 
 val sectionsThatMayPrecedeThis = mapOf<String, HashSet<String>>(
-	"Введение" to hashSetOf(""),
-	"Постановка задачи" to hashSetOf("Введение"),
-	"Обзор" to hashSetOf("Постановка задачи"),
-	"Контент" to hashSetOf("Обзор", "Контент"),
-	"Заключение" to hashSetOf("Контент"),
-	"Список литературы" to hashSetOf("Заключение")
+	Section.INTRODUCTION.title to hashSetOf(""),
+	Section.PROBLEM_STATEMENT.title to hashSetOf(Section.INTRODUCTION.title),
+	Section.REVIEW.title to hashSetOf(Section.PROBLEM_STATEMENT.title),
+	Section.CONTENT.title to hashSetOf(Section.REVIEW.title, Section.CONTENT.title),
+	Section.CONCLUSION.title to hashSetOf(Section.CONTENT.title),
+	Section.BIBLIOGRAPHY.title to hashSetOf(Section.CONCLUSION.title)
 )
 
 val RULE_SECTIONS_ORDER = TableOfContentRuleBuilder()
@@ -207,23 +209,23 @@ val RULE_SECTIONS_ORDER = TableOfContentRuleBuilder()
 			.filterNot { line ->
 				val words = line.text
 					.filter { it.text.trim().isNotEmpty() }
-					.filterNot { it.text.contains("[0-9]+\\.".toRegex()) }
-				words.isEmpty() || words[0].text == "Оглавление"
+					.filterNot { it.text.contains("[0-9]+\\.".toRegex()) }		// remove numbering
+				words.isEmpty() || words[0].text == Section.TABLE_OF_CONTENT.title
 			}
 			.filter { line ->
 				val words = line.text
 					.filter { it.text.trim().isNotEmpty() }
-					.filterNot { it.text.contains("[0-9]+\\.".toRegex()) }
+					.filterNot { it.text.contains("[0-9]+\\.".toRegex()) }		// remove numbering
 
 				val sectionName =
-					if (words[0].text == "Список" && words[1].text == "литературы" ||
-						words[0].text == "Постановка" && words[1].text == "задачи"
+					if ((words[0].text + " " + words[1].text) == Section.BIBLIOGRAPHY.title ||
+						(words[0].text + " " + words[1].text) == Section.PROBLEM_STATEMENT.title
 					)
 						words[0].text + " " + words[1].text
 					else if (sectionsThatMayPrecedeThis.contains(words[0].text))
 						words[0].text
 					else
-						"Контент"
+						Section.CONTENT.title
 
 				val isRuleViolation =
 					!sectionsThatMayPrecedeThis[sectionName]!!.contains(nameOfPreviousSection)
