e391620748e4b8ceaef9ea6728ae3cbb4e47c9e8
Darderion <Eobard1SA@gmail.com>
2022-05-22 10:35:25 +0300

    Added descriptions to server.sh and webapp.sh, added date to logs

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
-nohup java -jar app.jar > logs/server.log &
+date=$(date +"%T")
+nohup java -jar app.jar > logs/server_$date.log &
 pId=$!
-echo "kill $pId" > pause_server.sh
-echo "bash pause_app.sh && bash pause_server.sh" > map_pause.sh
+cat << EOF > pause_server.sh
+kill $pId || echo "SERVER WITH pID=$pId WAS NOT FOUND
+
+That could be caused by either:
+
+Multiple attempts of launching a server when it was already running
+In this case you can pause the server by running
+\\\$ kill \\\$(lsof -t -i :PORT)
+
+OR
+
+Server not launched
+You can locate the process by running
+\\\$ lsof -i :PORT
+
+where PORT is the port on which you're running the server
+Example:
+\\\$ lsof -i :8081
+
+You can also use this command to ensure that the port is available:
+
+\\\$ kill \\\$(lsof -t -i :PORT); bash server.sh"
+EOF
+
+echo "bash pause_app.sh; bash pause_server.sh" > map_pause.sh
 chmod u+x map_pause.sh
 chmod u+x pause_server.sh
