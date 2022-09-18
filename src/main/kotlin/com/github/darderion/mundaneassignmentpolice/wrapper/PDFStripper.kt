6dfea0697f9ed5c84b4558af53769036f18cf169
Pavel Saltykov <55807804+PavelSaltykov@users.noreply.github.com>
2022-09-18 10:58:26 +0300

    Fixed bug with ligatures (#38)
    * Fix error due to ligatures
    
    * Correct stringIndex

--- src/main/kotlin/com/github/darderion/mundaneassignmentpolice/wrapper/PDFStripper.kt
+++ src/main/kotlin/com/github/darderion/mundaneassignmentpolice/wrapper/PDFStripper.kt
@@ -3,9 +3,9 @@ package com.github.darderion.mundaneassignmentpolice.wrapper
 import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Coordinate
 import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Font
 import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Symbol
-import com.github.darderion.mundaneassignmentpolice.pdfdocument.text.Word
 import org.apache.pdfbox.text.PDFTextStripper
 import org.apache.pdfbox.text.TextPosition
+import java.text.Normalizer
 
 class PDFStripper: PDFTextStripper() {
 	val symbols: MutableList<Symbol> = mutableListOf()
@@ -14,15 +14,22 @@ class PDFStripper: PDFTextStripper() {
 	 * Override the default functionality of PDFTextStripper.writeString()
 	 */
 	override fun writeString(string: String?, textPositions: List<TextPosition>) {
-		for (text: TextPosition in textPositions) {
-			val symbol = text.unicode
-			if (symbol != null && symbol != " ") {
-				symbol.forEach {
+		var stringIndex = 0
+		for (textPosition in textPositions) {
+			val text = textPosition.unicode
+			val normalizedText = if (text == string!!.substring(stringIndex until (stringIndex + text.length)))
+				text
+			else
+				Normalizer.normalize(text, Normalizer.Form.NFKC) // same normalization form as used in PDFTextStripper
+
+			if (normalizedText != null && normalizedText != " ") {
+				normalizedText.forEach {
 					symbols.add(
-						Symbol(it, Font(text.fontSize), Coordinate(text.xDirAdj to text.yDirAdj))
+						Symbol(it, Font(textPosition.fontSize), Coordinate(textPosition.xDirAdj to textPosition.yDirAdj))
 					)
 				}
 			}
+			stringIndex += normalizedText.length
 			/*
 			words.add(text.unicode + " [(X=" + text.xDirAdj + ",Y=" +
 						text.yDirAdj + ") height=" + text.heightDir + " width=" +
