fbf67aee983a093ccf98541efd1b09c99644bd13
liana <liana300291@gmail.com>
2022-10-14 19:46:14 +0300

    added tests

--- src/test/kotlin/com/github/darderion/mundaneassignmentpolice/checker/RulesTests.kt
+++ src/test/kotlin/com/github/darderion/mundaneassignmentpolice/checker/RulesTests.kt
@@ -109,10 +109,19 @@ class RulesTests : StringSpec({
 	"Symbol rule should ignore usage of capital letters inside brackets that are placed after the '.' symbol" {
 		RULE_BRACKETS_LETTERS.process(PDFBox().getPDF(filePathBracketLetters)).count() shouldBeExactly 1
 	}
+	"Symbol rule should detect detect no space usage"{
+		RULE_NO_SPACE_AFTER_PUNCTUATION.process(PDFBox().getPDF(filePathNoSpaceAfterPunctuation )).count() shouldBeExactly 1
+	}
+	"Symbol rule should detect incorrect space usage"{
+		RULE_SPACE_BEFORE_PUNCTUATION.forEach { it.process(PDFBox().getPDF(filePathSpaceBeforePunctuation)).count() shouldBeExactly 1 }}
 	"RULE_OPENING_QUOTATION and RULE_CLOSING_QUOTATION should process multiple lines" {
 		RULE_OPENING_QUOTATION.process(PDFBox().getPDF(filePathMultilineCitation)).count() +
 				RULE_CLOSING_QUOTATION.process(PDFBox().getPDF(filePathMultilineCitation)).count() shouldBeExactly 0
 	}
+
+	"RULE_LONG_SENTENCE should detect long sentences without quotes"{
+			RULE_LONG_SENTENCE.process(PDFBox().getPDF(filePathLongSentence)).count() shouldBeExactly 3
+		}
 }) {
 	companion object {
 		const val filePathQuestionMarkAndDashes =
@@ -143,5 +152,11 @@ class RulesTests : StringSpec({
 			"${TestsConfiguration.resourceFolder}checker/BracketLetters.pdf"
 		const val filePathMultilineCitation =
 			"${TestsConfiguration.resourceFolder}checker/MultilineCitation.pdf"
+		const val filePathLongSentence =
+				"${TestsConfiguration.resourceFolder}checker/SentenceRuleTestsLongSentence.pdf"
+		const val  filePathSpaceBeforePunctuation =
+				"${TestsConfiguration.resourceFolder}checker/SymbolRuleTestsSpaceBeforePunctuation.pdf"
+		const val  filePathNoSpaceAfterPunctuation =
+				"${TestsConfiguration.resourceFolder}checker/SymbolRuleTestsNoSpaceAfterPunctuation.pdf"
 	}
 }
